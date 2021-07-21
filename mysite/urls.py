b30c2afcf81d00264c3aff60044aede569029ef7
jingjiu <jingjiu@qq.com>
2021-07-22 00:05:29 +0800

    Initial commit

---
+++ mysite/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from login import views
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('index/', views.index),
+    # path('login/', views.login),
+    # path('register/', views.register),
+    # path('logout/', views.logout),
+]
